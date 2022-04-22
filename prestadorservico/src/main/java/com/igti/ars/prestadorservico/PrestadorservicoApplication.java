bf5f6123012037f65a13a82e2979e878268bc271
jonatas.oliveira <jonatas.olivera>
2022-04-22 00:23:23 -0300

    First commit

---
+++ prestadorservico/src/main/java/com/igti/ars/prestadorservico/PrestadorservicoApplication.java
@@ -0,0 +1,13 @@
+package com.igti.ars.prestadorservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestadorservicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrestadorservicoApplication.class, args);
+    }
+
+}
---
+++ prestadorservico/src/test/java/com/igti/ars/prestadorservico/PrestadorservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igti.ars.prestadorservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestadorservicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
